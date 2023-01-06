5dba7278993eabd5bdb97f0bb2003339a02ec596
Anonymous6214 <david20040730@gmail.com>
2023-01-06 20:43:44 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,16 @@
 Import-Module posh-git
 Import-Module Terminal-Icons
 Import-Module z
+function title {
+    param (
+        $name
+    )
+    $host.UI.RawUI.WindowTitle = $name
+}
+$env:PNPPOWERSHELL_UPDATECHECK="false"
+$env:PYTHONIOENCODING='utf-8'
+$ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
+$ENV:STARSHIP_DISTRO = "者 "
+Invoke-Expression (&starship init powershell)
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussel.omp.json" | Invoke-Expression
