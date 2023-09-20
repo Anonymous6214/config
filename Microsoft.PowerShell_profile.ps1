968ae7dfdd4a575b75320b93db14a3b86dbea5ea
Anonymous6214 <david20040730@gmail.com>
2023-09-20 14:53:12 +0800

    updata pwsh

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module posh-git
-Import-Module Terminal-Icons
-Import-Module z
-$env:PNPPOWERSHELL_UPDATECHECK="false"
-$env:PYTHONIOENCODING='utf-8'
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
