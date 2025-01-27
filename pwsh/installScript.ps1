2b96dd2f6e2cd290456944944eb0e3a5969fc227
Anonymous6214 <david20040730@gmail.com>
2025-01-28 00:42:21 +0800

    add install instruction

--- pwsh/installScript.ps1
+++ pwsh/installScript.ps1
@@ -2,4 +2,5 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 Install-Module posh-git -Scope CurrentUser -Force
 Install-Module -Name Terminal-Icons -Repository PSGallery
 Install-Module z -AllowClobber
+winget install --id Git.Git -e --source winget
 winget install JanDeDobbeleer.OhMyPosh -s winget
---
+++ pwsh/readmd.md
@@ -0,0 +1,5 @@
+1. install firacode
+2. install powershell
+3. run `installScript.ps1`
+4. replace terminal config with setting.json
+5. run `notepad $profile` and paste Microsoft.PowerShell_profile.ps1
