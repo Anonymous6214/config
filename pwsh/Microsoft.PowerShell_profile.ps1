a2cd4a8f180e297d78c71e62722ce6cf9cabf9f4
Anonymous6214 <david20040730@gmail.com>
2025-05-23 16:36:12 +0800

    a

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,11 @@
 Import-Module posh-git
 Import-Module Terminal-Icons
 Import-Module z
+Import-Module Az.Tools.Predictor
 $env:PNPPOWERSHELL_UPDATECHECK='false'
 $env:PYTHONIOENCODING='utf-8'
-$env:PIPENV_VENV_IN_PROJECT=1
 $env:POETRY_VIRTUALENVS_IN_PROJECT=1
+$env:PIPENV_VENV_IN_PROJECT=1
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadLineOption -PredictionViewStyle ListView
 New-Alias -Name v -Value nvim
